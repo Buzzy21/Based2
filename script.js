d2bbe657cfe05f799603cce5dc7d4a381e4f17c9
William Liu <lursteddev@gmail.com>
2025-06-21 19:05:11 -0400

    Implement basic one-line addition

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputField = document.getElementById("input");
+
+// Note that currently the text box only works for one calculation at once
+inputField.addEventListener("input",()=> {
+    const inputString = inputField.value;
+    
+    let termA = termB = "";
+    
+    let i = 0;
+    let grabTerm = () => {
+        let term = "";
+        for(; i < inputString.length && (!isNaN(inputString[i]) || inputString[i] == ' '); i++) {
+            if(!isNaN(inputString[i])) term += inputString[i];
+        }
+        return term;
+    }
+
+    termA = grabTerm();
+
+    if(i >= inputString.len) return;
+    let operator = inputString[i];
+
+    i++;
+    
+    termB = grabTerm();
+
+    if(operator=='+') {
+        console.log("Results:",parseInt(termA)+parseInt(termB));
+    }
+    // console.log("Term A:",termA," | Operator:",operator," | Term B",termB);
+});
